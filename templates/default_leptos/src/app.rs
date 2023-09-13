984a3d17000fa50098c4ffceddf40845976f3652
mrvillage <josef@mrvillage.dev>
2023-09-13 11:17:11 -0400

    Fix app.rs in default leptos template

--- templates/default_leptos/src/app.rs
+++ templates/default_leptos/src/app.rs
@@ -1,15 +1,13 @@
 use leptos::*;
 use leptos_dom::IntoView;
 use leptos_meta::{provide_meta_context, Stylesheet, Title};
-use leptos_use::{use_interval_fn_with_options, utils::Pausable, UseIntervalFnOptions};
-use mrvillage_ui::{Button, ButtonColor, NumberInput};
 
 #[component]
 pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
-        <Stylesheet id="leptos" href="/pkg/timer.css" />
+        <Stylesheet id="leptos" href="/pkg/$$-cli-crate-name-$$.css" />
         <Stylesheet id="mrvillage-ui" href="/mrvillage-ui.css" />
         <Title text="Timer" />
         <div class="dark">
